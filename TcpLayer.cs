1dbaf774ea64015ae88b274911b5efb5dfd8dff3
RockerL <78175054@qq.com>
2018-06-30 11:06:24 +0800

    提高tcplayer的可读性

--- TcpLayer.cs
+++ TcpLayer.cs
@@ -10,12 +10,18 @@ using UnityEngine;
 
 namespace U3DUtility
 {
+    /// <summary>
+    /// 接收数据包结构
+    /// </summary>
     public struct Pkt
     {
-        public short messId;
-        public byte[] data;
+        public short messId;    //消息id
+        public byte[] data;     //序列化后的数据
     }
 
+    /// <summary>
+    /// 数据传输工具类
+    /// </summary>
     public class TcpLayer : MonoBehaviour
     {
         class AsyncData
@@ -32,69 +38,84 @@ namespace U3DUtility
         public delegate void OnDisconnectEvent(string msg);
         public delegate void OnRecvEvent(int msgId, byte[] data);
 
-        private TcpClient m_TcpClient;
-        private NetworkStream m_NetStream = null;
-        private bool m_IsConnected = false;
-        private OnConnectEvent m_OnConnect;
-        private OnDisconnectEvent m_OnDisConnect;
-        private OnRecvEvent m_OnRecvPackage;
-        private string m_IP;
-        private int m_Port;
-        private int m_SendBuffSize = 10240;
-        private int m_RecvBuffSize = 10240;
+        private TcpClient mTcpClient;
+        private NetworkStream mNetStream = null;
+        private bool mIsConnected = false;
+        private OnConnectEvent mOnConnect;
+        private OnDisconnectEvent mOnDisConnect;
+        private OnRecvEvent mOnRecvPackage;
 
-        private Queue<Pkt> m_RecvPacks = new Queue<Pkt>();
+        //下面几个参数是用来重连时使用的参数
+        private string mIP;     //当前连接的服务器IP
+        private int mPort;      //当前连接的端口
+        private int mSendBuffSize = 10240;  //发送缓冲大小
+        private int mRecvBuffSize = 10240;  //接受缓冲大小
 
-        private static TcpLayer m_Singleton = null;
+        private Queue<Pkt> mRecvPacks = new Queue<Pkt>();   //接收包暂存队列
 
-        public static TcpLayer Singleton
+        private static TcpLayer mSingleton = null;
+
+        public static TcpLayer singleton
         {
             get
             {
-                if (m_Singleton == null)
+                if (mSingleton == null)
                 {
                     Loom.Initialize();
 
                     GameObject o = new GameObject("Tcp Connector");
                     DontDestroyOnLoad(o);
-                    m_Singleton = o.AddComponent<TcpLayer>();
+                    mSingleton = o.AddComponent<TcpLayer>();
                 }
 
-                return m_Singleton;
+                return mSingleton;
             }
         }
 
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        /// <param name="recvBuffSize">Socket接收缓冲区大小</param>
+        /// <param name="sendBuffSize">Socket发送缓冲区大小</param>
         public void Init (int recvBuffSize, int sendBuffSize)
         {
-            m_SendBuffSize = sendBuffSize;
-            m_RecvBuffSize = recvBuffSize;
+            mSendBuffSize = sendBuffSize;
+            mRecvBuffSize = recvBuffSize;
         }
 
+        /// <summary>
+        /// 新建连接
+        /// </summary>
+        /// <param name="ip">连接的服务器IP</param>
+        /// <param name="port">连接的端口号</param>
+        /// <param name="connEvent">连接完成后回调</param>
+        /// <param name="disconnEvent">断开连接发生时回调</param>
+        /// <param name="recvEvent">接收到消息包时回调</param>
         public void Connect(string ip, int port, OnConnectEvent connEvent, OnDisconnectEvent disconnEvent, OnRecvEvent recvEvent)
         {
-            if (m_IsConnected)
+            if (mIsConnected)
             {
                 Disconnect("reconnect");
             }
 
-            m_OnConnect = connEvent;
-            m_OnDisConnect = disconnEvent;
-            m_OnRecvPackage = recvEvent;
-            m_IP = ip;
-            m_Port = port;
+            mOnConnect = connEvent;
+            mOnDisConnect = disconnEvent;
+            mOnRecvPackage = recvEvent;
+            mIP = ip;
+            mPort = port;
 
-            m_TcpClient = new TcpClient
+            mTcpClient = new TcpClient
             {
                 NoDelay = true,
-                ReceiveBufferSize = m_RecvBuffSize,
-                SendBufferSize = m_SendBuffSize
+                ReceiveBufferSize = mRecvBuffSize,
+                SendBufferSize = mSendBuffSize
             };
 
-            m_IsConnected = false;
+            mIsConnected = false;
 
             try
             {
-                m_TcpClient.BeginConnect(IPAddress.Parse(ip), port, new AsyncCallback(OnConnectCallback), m_TcpClient);
+                mTcpClient.BeginConnect(IPAddress.Parse(ip), port, new AsyncCallback(OnConnectCallback), mTcpClient);
 
                 Invoke("ConnectTimeOutCheck", 3);
             }
@@ -105,37 +126,48 @@ namespace U3DUtility
                     CancelInvoke("ConnectTimeOutCheck");
                 }
 
-                m_OnConnect?.Invoke(false, ex.ToString());
+                mOnConnect?.Invoke(false, ex.ToString());
             }
         }
 
+        /// <summary>
+        /// 添加一个新的接收数据回调
+        /// </summary>
+        /// <param name="recvEvent">要添加的回调函数</param>
         public void AddRecvEvent (OnRecvEvent recvEvent)
         {
-            m_OnRecvPackage += recvEvent;
+            mOnRecvPackage += recvEvent;
         }
 
+        /// <summary>
+        /// 移除一个接受数据回调
+        /// </summary>
+        /// <param name="recvEvent">要移除的回调函数</param>
         public void RemoveRecvEvent(OnRecvEvent recvEvent)
         {
-            m_OnRecvPackage -= recvEvent;
+            mOnRecvPackage -= recvEvent;
         }
 
+        /// <summary>
+        /// 发起重新连接
+        /// </summary>
         public void Reconnect()
         {
-            if (m_IsConnected)
+            if (mIsConnected)
             {
                 Disconnect("reconnect");
             }
 
-            m_TcpClient = new TcpClient
+            mTcpClient = new TcpClient
             {
                 NoDelay = true,
-                ReceiveBufferSize = m_RecvBuffSize,
-                SendBufferSize = m_SendBuffSize
+                ReceiveBufferSize = mRecvBuffSize,
+                SendBufferSize = mSendBuffSize
             };
 
             try
             {
-                m_TcpClient.BeginConnect(IPAddress.Parse(m_IP), m_Port, new AsyncCallback(OnConnectCallback), m_TcpClient);
+                mTcpClient.BeginConnect(IPAddress.Parse(mIP), mPort, new AsyncCallback(OnConnectCallback), mTcpClient);
 
                 Invoke("ConnectTimeOutCheck", 3);
             }
@@ -146,27 +178,36 @@ namespace U3DUtility
                     CancelInvoke("ConnectTimeOutCheck");
                 }
 				
-                m_OnConnect?.Invoke(false, ex.ToString());
+                mOnConnect?.Invoke(false, ex.ToString());
             }
         }
 
+        /// <summary>
+        /// 主动断开连接
+        /// </summary>
+        /// <param name="msg">断开原因字符串</param>
         public void Disconnect(string msg)
         {
-            if (m_IsConnected)
+            if (mIsConnected)
             {
-                m_NetStream.Close();
-                m_TcpClient.Close();
-                m_IsConnected = false;
+                mNetStream.Close();
+                mTcpClient.Close();
+                mIsConnected = false;
 
-                m_OnDisConnect?.Invoke(msg);
+                mOnDisConnect?.Invoke(msg);
 
-                lock (m_RecvPacks)
+                lock (mRecvPacks)
                 {
-                    m_RecvPacks.Clear();
+                    mRecvPacks.Clear();
                 }
             }
         }
 
+        /// <summary>
+        /// 发送数据包到服务器
+        /// </summary>
+        /// <param name="messId">数据包消息id</param>
+        /// <param name="data">数据包内容</param>
         public void SendPack(short messId, byte[] data)
         {
             int length = data.Length + PACK_HEAD_SIZE + MSG_ID_SIZE;
@@ -185,7 +226,7 @@ namespace U3DUtility
 
             try
             {
-                m_NetStream.Write(sendBytes, 0, length);
+                mNetStream.Write(sendBytes, 0, length);
             }
             catch (Exception ex)
             {
@@ -193,6 +234,10 @@ namespace U3DUtility
             }
         }
 
+        /// <summary>
+        /// 当连接完成后回调处理
+        /// </summary>
+        /// <param name="asyncResult">异步结果</param>
         void OnConnectCallback(IAsyncResult asyncResult)
         {
             try
@@ -208,58 +253,58 @@ namespace U3DUtility
             {
                 U3DUtility.Loom.QueueOnMainThread(() =>
                 {
-                    m_OnConnect?.Invoke(false, ex.ToString());
+                    mOnConnect?.Invoke(false, ex.ToString());   //发生了异常，通知连接失败
                 });
             }
             finally
             {
-                m_NetStream = m_TcpClient.GetStream();
+                mNetStream = mTcpClient.GetStream();
 
-                BeginPackRead();
+                BeginPackRead(); //开始异步读取包
 
-                U3DUtility.Loom.QueueOnMainThread(() =>
+                U3DUtility.Loom.QueueOnMainThread(() => //到主线程中通知连接成功
                 {
                     if (IsInvoking("ConnectTimeOutCheck"))
                     {
                         CancelInvoke("ConnectTimeOutCheck");
                     }
 
-                    m_IsConnected = true;
-                    m_OnConnect?.Invoke(true, "");
+                    mIsConnected = true;
+                    mOnConnect?.Invoke(true, "");
                 });
             }
         }
 
         void Update()
         {
-            //处理所有接收的包
-            lock(m_RecvPacks)
+            //通知所有注册的回调函数处理当前接收的包
+            lock(mRecvPacks)
             {
-                for (; m_RecvPacks.Count > 0;)
+                for (; mRecvPacks.Count > 0;)
                 {
-                    var pkt = m_RecvPacks.Dequeue();
-                    m_OnRecvPackage?.Invoke(pkt.messId, pkt.data);
+                    var pkt = mRecvPacks.Dequeue();
+                    mOnRecvPackage?.Invoke(pkt.messId, pkt.data);
                 }
             }
         }
 
         void ConnectTimeOutCheck()
         {
-            if (!m_IsConnected)
+            if (!mIsConnected)
             {
-                m_OnConnect?.Invoke(false, "connect time out");
+                mOnConnect?.Invoke(false, "connect time out");
             }
         }
 
         /// <summary>
-        /// 接收包头的异步回调
+        /// 读取包头的异步回调
         /// </summary>
         /// <param name="asyncResult">异步参数</param>
         void ReadAsyncCallBackPackHead(IAsyncResult asyncResult)
         {
             try
             {
-                int dataLen = m_NetStream.EndRead(asyncResult);
+                int dataLen = mNetStream.EndRead(asyncResult);
 
                 AsyncData head_data = (AsyncData)asyncResult.AsyncState;
 
@@ -270,33 +315,33 @@ namespace U3DUtility
 
                     //Debug.LogFormat("recv head {0} {1} {2}", dataLen, packLen, msgID);
 
-                    if (packLen == MSG_ID_SIZE)
+                    if (packLen == MSG_ID_SIZE) //表示包体没有数据，只有一个消息ID，这时发起新的包异步读取
                     {
-                        BeginPackRead();
+                        BeginPackRead();    
                     }
                     else if (packLen < MSG_ID_SIZE)
                     {
-                        throw new Exception("recv pack len " + packLen);
+                        throw new Exception("recv pack len " + packLen); //服务器发送过来一个错误的包大小
                     }
-                    else
+                    else //发起异步读取包体
                     {
                         AsyncData pack_data = new AsyncData
                         {
-                            buff = new byte[packLen - MSG_ID_SIZE],
+                            buff = new byte[packLen - MSG_ID_SIZE], //计算包体大小需要减掉消息id所占的2个字节
                             pos = 0,
                             messId = msgID
                         };
 
-                        m_NetStream.BeginRead(pack_data.buff, 0, pack_data.buff.Length, new AsyncCallback(ReadAsyncCallBackPack), pack_data);
+                        mNetStream.BeginRead(pack_data.buff, 0, pack_data.buff.Length, new AsyncCallback(ReadAsyncCallBackPack), pack_data);
                     }
                 }
-                else //没读取完则继续读取
+                else //没读取完则继续读取包头剩余部分
                 {
                     head_data.pos += dataLen;
 
                     Debug.LogFormat("continue recv head {0} {1}", head_data.buff.Length, head_data.pos);
 
-                    m_NetStream.BeginRead(head_data.buff, head_data.pos, head_data.buff.Length - head_data.pos, new AsyncCallback(ReadAsyncCallBackPackHead), head_data);
+                    mNetStream.BeginRead(head_data.buff, head_data.pos, head_data.buff.Length - head_data.pos, new AsyncCallback(ReadAsyncCallBackPackHead), head_data);
                 }
             }
             catch (Exception ex)
@@ -310,11 +355,15 @@ namespace U3DUtility
             }
         }
 
+        /// <summary>
+        /// 异步读取包体回调函数
+        /// </summary>
+        /// <param name="asyncResult">读取结果</param>
         void ReadAsyncCallBackPack(IAsyncResult asyncResult)
         {
             try
             {
-                int dataLen = m_NetStream.EndRead(asyncResult);
+                int dataLen = mNetStream.EndRead(asyncResult);
 
                 AsyncData data = (AsyncData)asyncResult.AsyncState;
 
@@ -324,11 +373,11 @@ namespace U3DUtility
                     p.data = data.buff;
                     p.messId = data.messId;
 
-                    lock(m_RecvPacks)
+                    lock(mRecvPacks)
                     {
                         //Debug.LogFormat("recv data {0} {1}", data.buff.Length, p.messId);
 
-                        m_RecvPacks.Enqueue(p);
+                        mRecvPacks.Enqueue(p);
                     }
 
                     BeginPackRead();
@@ -339,7 +388,7 @@ namespace U3DUtility
 
                     //Debug.LogFormat("continue recv data {0} {1}", data.buff.Length, data.pos);
 
-                    m_NetStream.BeginRead(data.buff, data.pos, data.buff.Length - data.pos, new AsyncCallback(ReadAsyncCallBackPack), data);
+                    mNetStream.BeginRead(data.buff, data.pos, data.buff.Length - data.pos, new AsyncCallback(ReadAsyncCallBackPack), data);
                 }
             }
             catch (Exception ex)
@@ -351,17 +400,20 @@ namespace U3DUtility
             }
         }
 
+        /// <summary>
+        /// 开始异步读取包
+        /// </summary>
         void BeginPackRead()
         {
             AsyncData data = new AsyncData
             {
-                buff = new byte[PACK_HEAD_SIZE + MSG_ID_SIZE],
+                buff = new byte[PACK_HEAD_SIZE + MSG_ID_SIZE], //包大小加上ID一共6个字节
                 pos = 0
             };
 
             try
             {
-                m_NetStream.BeginRead(data.buff, 0, data.buff.Length, new AsyncCallback(ReadAsyncCallBackPackHead), data);
+                mNetStream.BeginRead(data.buff, 0, data.buff.Length, new AsyncCallback(ReadAsyncCallBackPackHead), data);
             }
             catch (Exception ex)
             {
